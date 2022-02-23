0da700da59546b19f47e430f559b4556b2ecbe1a
SoesLy <90309107+SoesLy@users.noreply.github.com>
2022-02-23 20:58:20 +0100

    Add files via upload

---
+++ Tabloid/src/main/java/com/example/tabloid/TabloidApplication.java
@@ -0,0 +1,13 @@
+package com.example.tabloid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TabloidApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TabloidApplication.class, args);
+    }
+
+}
---
+++ Tabloid/src/test/java/com/example/tabloid/TabloidApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tabloid;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TabloidApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
